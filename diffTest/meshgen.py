a08b2f51e163c72e064115875a84ea8ceb8e8ee1
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-09-20 15:51:54 +0100

    properly update the profile and remove some redundant stuff

--- diffTest/meshgen.py
+++ diffTest/meshgen.py
@@ -1,6 +1,8 @@
 import numpy as N
+import vtk
+
 # Layout of vertices
-# 
+#
 # -------in+n-1-------in+0-------in+1
 #         /  \        /  \        / \
 # \      /    \      /    \      /   \
@@ -10,56 +12,58 @@ import numpy as N
 # (i-1)n+n-1--(i-1)n+0----(i-1)+1--------
 
 
-r = 3.
-length = 110.
+r = 3.0
+length = 110.0
 
 n = 48
 
-alpha = 2*N.pi/n
+alpha = 2 * N.pi / n
 
-side = 2 * r * N.sin(alpha/2.)
-targetHeight = side*N.sin(N.pi/3.)
+side = 2 * r * N.sin(alpha / 2.0)
+targetHeight = side * N.sin(N.pi / 3.0)
 
-nRows = int(length/targetHeight)
-height = length/nRows
+nRows = int(length / targetHeight)
+height = length / nRows
 
-zs = N.mgrid[-length/2.:length/2.:(nRows+1)*1j]
-thetas = N.mgrid[0:2*N.pi:alpha]
+zs = N.mgrid[-length / 2.0 : length / 2.0 : (nRows + 1) * 1j]
+thetas = N.mgrid[0 : 2 * N.pi : alpha]
 
 shift = 0
-points = N.zeros((len(zs)*len(thetas), 3))
-triangles = []
+
+pd = vtk.vtkPolyData()
+points = vtk.vtkPoints()
+points.Allocate(len(zs) * len(thetas))
+pd.SetPoints(points)
+
+triangles = pd.GetPolys()
+
 p = 0
 for i, z in enumerate(zs):
     for j, theta in enumerate(thetas):
-        points[p, 0] = r*N.cos(theta+shift)
-        points[p, 1] = r*N.sin(theta+shift)
-        points[p, 2] = z
+        assert (
+            points.InsertNextPoint(
+                r * N.cos(theta + shift), r * N.sin(theta + shift), z
+            )
+            == p
+        )
         if i >= 1:
-            if j>0:
-                triangles.append( (p, p-1, p-n) )
+            if j > 0:
+                triangles.InsertNextCell(3, (p, p - 1, p - n))
                 pass
-            if j < n-1:
-                triangles.append( (p, p-n, p-n+1) )
+            if j < n - 1:
+                triangles.InsertNextCell(3, (p, p - n, p - n + 1))
             else:
-                triangles.append( (p, p-n, p-2*n+1) )
-                triangles.append( (p, p-n+1, p-2*n+1) )
+                triangles.InsertNextCell(3, (p, p - n, p - 2 * n + 1))
+                triangles.InsertNextCell(3, (p, p - n + 1, p - 2 * n + 1))
                 pass
             pass
-        
+
         p += 1
         continue
-    shift += alpha/2.
+    shift += alpha / 2.0
     continue
 
-triangles = N.array(triangles)
-
-from enthought.tvtk.api import tvtk as vtk
-pd = vtk.PolyData()
-pd.points = points
-pd.polys = triangles
-
-stl = vtk.STLWriter()
-stl.file_name = 'cyl.stl'
-stl.input = pd
-stl.write()
+stl = vtk.vtkSTLWriter()
+stl.SetFileName("cyl.stl")
+stl.SetInputData(pd)
+stl.Write()
