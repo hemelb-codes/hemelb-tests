e1a21a0112a1a62a35f319cd93f80c980468e509
Rupert Nash <r.nash@epcc.ed.ac.uk>
2022-05-29 08:02:22 +0100

    make mpirun configurable by env vars

--- diffTest/diffTest.sh
+++ diffTest/diffTest.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+MPIRUN=${MPIRUN:-mpirun}
+MPIRUN_FLAGS=${MPIRUN_FLAGS:-}
+MPIRUN_NUMPROCS_FLAG=${MPIRUN_NUMPROCS_FLAG:--np}
+NUMPROCS=${NUMPROCS:-3}
+
 # Remove any results folder so that we have somewhere for the test output to go
 rm -rf results
 
 # Run the test (hemelb needs to be in your PATH)
-mpirun -np 3 hemelb -in config.xml -i 1 -ss 1111
+$MPIRUN $MPIRUN_FLAGS $MPIRUN_NUMPROCS_FLAG $NUMPROCS hemelb -in config.xml -i 1 -ss 1111
 
 # Use the script to examine any differences between the snapshot files
 ./NumericalComparison CleanExtracted results/Extracted
--- fluid-checkpoint/run.sh
+++ fluid-checkpoint/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -ex
 
+MPIRUN=${MPIRUN:-mpirun}
+MPIRUN_FLAGS=${MPIRUN_FLAGS:-}
+MPIRUN_NUMPROCS_FLAG=${MPIRUN_NUMPROCS_FLAG:--np}
+NUMPROCS=${NUMPROCS:-3}
 
 for name in whole first_half second_half; do
     echo "Running $name"
     rm -rf $name
     hemelb-confcheck $name.xml
-    mpirun -np 3 hemelb -in $name.xml -out $name -i 1 -ss 1111
+    $MPIRUN $MPIRUN_FLAGS $MPIRUN_NUMPROCS_FLAG $NUMPROCS hemelb -in $name.xml -out $name -i 1 -ss 1111
 done
 
