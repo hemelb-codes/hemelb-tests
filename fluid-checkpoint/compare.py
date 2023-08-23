d6032e5392ded6753390f0d934427aaf10f21f3f
Rupert Nash <r.nash@epcc.ed.ac.uk>
2023-08-23 15:42:31 +0100

    Update fluid checkpoint to restart from CP xml

--- fluid-checkpoint/compare.py
+++ fluid-checkpoint/compare.py
@@ -14,15 +14,13 @@ def get_cp(run_name, time):
 
 whole1 = get_cp("whole", 100)
 whole2 = get_cp("whole", 200)
-first1 = get_cp("first_half", 100)
 second1 = get_cp("second_half", 100)
 second2 = get_cp("second_half", 200)
 
 w1 = whole1.GetByTimeStep(100)
-f1 = first1.GetByTimeStep(100)
 s1 = second1.GetByTimeStep(100)
 
-assert np.all(w1 == f1) and np.all(w1 == s1)
+assert np.all(w1 == s1)
 
 print("First checkpoint matches")
 
--- fluid-checkpoint/run.sh
+++ fluid-checkpoint/run.sh
@@ -6,11 +6,18 @@ MPIRUN_FLAGS=${MPIRUN_FLAGS:-}
 MPIRUN_NUMPROCS_FLAG=${MPIRUN_NUMPROCS_FLAG:--np}
 NUMPROCS=${NUMPROCS:-3}
 
-for name in whole first_half second_half; do
-    echo "Running $name"
-    rm -rf $name
-    hemelb-confcheck $name.xml
-    $MPIRUN $MPIRUN_FLAGS $MPIRUN_NUMPROCS_FLAG $NUMPROCS hemelb -in $name.xml -out $name
-done
+function run_hemelb {
+    in_xml=$1
+    out_dir=$2
+    rm -rf $out_dir
+    hemelb-confcheck $in_xml
+    $MPIRUN $MPIRUN_FLAGS $MPIRUN_NUMPROCS_FLAG $NUMPROCS hemelb -in $in_xml -out $out_dir
+}
+
+echo "Running whole 200 steps"
+run_hemelb whole.xml whole
+
+echo "Running second half"
+run_hemelb whole/Checkpoints/100/restart.xml second_half
 
 python compare.py
